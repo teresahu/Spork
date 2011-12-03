33773b25d8b285cd090c149110a468178e3455f0
Paul Dewey <calibreneguru@gmail.com>
2011-12-03 10:34:41 -0500

    Added the admin file

---
+++ Spork/sporkapp/admin.py
@@ -0,0 +1,6 @@
+from sporkapp.models import Provider, Receiver, Donation
+from django.contrib import admin
+
+admin.site.register(Provider)
+admin.site.register(Receiver)
+admin.site.register(Donation)

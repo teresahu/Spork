accfa6062d1d7ae600f072055edf94f96139b999
Darren Woodley <hodgechen@gmail.com>
2011-12-03 07:24:06 -0500

    (Hopefully) fixed django version issue. Pray.

--- Spork/urls.py
+++ Spork/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Spork.views.home', name='home'),
-    # url(r'^Spork/', include('Spork.foo.urls')),
+    # Example:
+    # (r'^Spork/', include('Spork.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # (r'^admin/', include(admin.site.urls)),
 )

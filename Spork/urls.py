942b1b52fc1639b43ca921d7eb803a3d2e6f32b2
Darren Woodley <hodgechen@gmail.com>
2011-12-03 11:00:01 -0500

    cleaned up ugliness in urls.py

--- Spork/urls.py
+++ Spork/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-=======
 from django.contrib import admin
 admin.autodiscover()
 

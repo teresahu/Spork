fcb53f22d24dbc12db5da6510ab2bed438e6f3c4
Darren Woodley <darren@sporkatl.com>
2012-01-15 13:19:42 -0500

    removed MailChimp API key for github push

--- Spork/sporkapp/forms.py
+++ Spork/sporkapp/forms.py
@@ -3,9 +3,9 @@ from django import forms
 from sporkapp.models import Donation
 
 class SubscribeForm(forms.Form):
-    email = forms.EmailField(required=True)
-    firstname = forms.CharField(max_length = 30)
-    lastname = forms.CharField(max_length = 30)
+    email_address = forms.EmailField(required=True)
+    firstname = forms.CharField(max_length = 30, required=False)
+    lastname = forms.CharField(max_length = 30, required=False)
 
 class NewDonationForm(forms.ModelForm):
     class Meta:
--- Spork/sporkapp/views.py
+++ Spork/sporkapp/views.py
@@ -1,11 +1,37 @@
+import json
+import urllib
+
 from django.template import Context, loader
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
 
 from sporkapp.models import Donation
 from sporkapp.forms import NewDonationForm, SubscribeForm
 
 # Create your views here.
+def subscribe(request):
+    MAILCHIMP_API_KEY = ''
+    MAILCHIMP_API_ENDPOINT_URL = 'http://us4.api.mailchimp.com/1.3/?method=listSubscribe'
+    MAILCHIMP_LIST_ID = ''
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            api_params = {
+                'apikey': MAILCHIMP_API_KEY,
+                'id' : MAILCHIMP_LIST_ID,
+                'email_address' : form.cleaned_data['email_address']
+            }
+            result = urllib.urlopen(MAILCHIMP_API_ENDPOINT_URL, json.dumps(api_params))
+            result_json = json.loads(result.readlines())
+            if result_json == True:
+                return render_to_response('subscribe_thanks.html', context_instance=RequestContext(request))
+        else:
+            return HttpResponseRedirect('/home/')
+    else:
+        subscribe_form = SubscribeForm()
+        return render_to_response('subscribe.html', {'subscribe_form' : subscribe_form}, context_instance=RequestContext(request))
+
 def home(request):
     donations = Donation.objects.all()
     subscribe_form = SubscribeForm()
